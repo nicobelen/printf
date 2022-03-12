556d94b53db1dfe3adb983857fe8f4fce54f86a0
nicobelen <nicolasbelen93@gmail.com>
2022-03-12 09:51:35 -0800

    function print number

--- functions-1.c
+++ functions-1.c
@@ -23,7 +23,7 @@ int _printf_c(char caracter)
 int _printf_s(char *str)
 {
         int i = 0;
-        while (*(str + i) != 0)
+        while (*(str + i) != '\0')
         {
                 _putchar(*(str + i));
                 i++;
@@ -59,4 +59,32 @@ void print_rev(char *s)
 	}
 
 	_putchar('\n');
+}
+/**
+* print_number - function that prints an int
+* @n: parameter
+* Return: void
+*/
+void print_number(int n)
+{
+	unsigned int a;
+
+	if (n < 0)
+	{		
+		n = -n;
+		_putchar('-');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	while (n > 0)
+	{
+		n = ((n % 10) + '0');
+		n /= 10;
+		a = n;
+		a = ((n % 10) + '0');
+		a /= 10;
+		_putchar(a);
+	}
 }
