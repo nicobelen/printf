c5d9fba67b10e5d94e16e2c290615aac0fc2119e
Vinill <danimuller758@gmail.com>
2022-03-14 22:26:28 +0000

    Fixed betty putchar

--- _printf.c
+++ _printf.c
@@ -9,26 +9,14 @@
 
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-	if (format == NULL)
-		return (-1);
-=======
-/*Si escribieron algo dentro de format*/
->>>>>>> abdd23e30326594ae0657af4636ca29258968af4
-
 	va_list list;
 	int (*f)(va_list);
-<<<<<<< HEAD
+	int i = 0, j = 0;
 
 	va_start(list, format);
-	int i = 0, j = 0;
-=======
-	int i = 0, j = 0;
-	va_start (list, format);
 
 	if (format == NULL)
 		return (-1);
->>>>>>> abdd23e30326594ae0657af4636ca29258968af4
 
 	while (format[i] != '\0')
 	{
@@ -36,39 +24,25 @@ int _printf(const char *format, ...)
 		{
 			_putchar(format[i]);
 			j++;
-			i++;
-		}
+			i++;	}
 		if (format[i] == '\0')
 			return (j);
-
 		f = get_op_func(&format[i + 1]);
-
 		if (f != NULL)
 		{
-			j += f(list	);
+			j += f(list);
 			i += 2;
-			continue;
-		}
-<<<<<<< HEAD
+			continue;	}
 		if (format[i + 1] == '\0')
 			return (-1);
 		_putchar(format[i]);
-=======
-
-	    if (format[i + 1] == '\0')
-			return (-1);
-
-		_putchar(format[i]);
-        
->>>>>>> abdd23e30326594ae0657af4636ca29258968af4
 		j++;
+
 		if (format[i + 1] == '%')
 		{
-			i += 2;
-		}
+			i += 2;	}
 		else
-			i++;
-	}
+			i++;	}
 	va_end(list);
 	return (j);
 }
