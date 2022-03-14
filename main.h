947f2dbb5daebefe206f137f93a0e5bb56d0eb0e
nicobelen <nicolasbelen93@gmail.com>
2022-03-14 14:08:32 -0700

    created test folder

--- main.h
+++ main.h
@@ -4,19 +4,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-
-typedef struct op
+/**
+* struct op_var - structure of the list
+* @op: format
+*
+*/
+typedef struct op_var
 {
   char *op;
   int (*f)(va_list);
 } op_t;
 
+int (*get_op_func(const char *format))(va_list);
 int _printf(const char *format, ...);
 int _putchar(char c);
 int op_rev(va_list r);
 int op_str(va_list s);
 int op_char(va_list c);
 int op_d_i(va_list d);
-int (*get_op_func(const char *format))(va_list);
 
 #endif
